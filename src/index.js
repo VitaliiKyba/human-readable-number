afbfe232b40d970c4fe2475aa3c9bfbf31cac927
VitaliiKyba <k190583@gmail.com>
2021-03-12 22:32:35 +0200

    feat: add support units

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const units = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+    ];
+    if ((number.length = 1)) {
+        return units[number];
+    }
+};
